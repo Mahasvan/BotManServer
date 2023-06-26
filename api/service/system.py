ef7da9bb30682a9c7fcf53639c8c9b273ab8e5d3
Mahas1 <mahasvan@gmail.com>
2023-06-26 19:04:36 +0530

    formatting

--- api/service/system.py
+++ api/service/system.py
@@ -10,12 +10,12 @@ def get_processor_name():
         return platform.processor()
     elif platform.system() == "Darwin":
         os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
-        command ="sysctl -n machdep.cpu.brand_string"
+        command = "sysctl -n machdep.cpu.brand_string"
         return subprocess.check_output(command, shell=True).decode().strip()
     elif platform.system() == "Linux":
         command = "cat /proc/cpuinfo"
         all_info = subprocess.check_output(command, shell=True).decode().strip()
         for line in all_info.split("\n"):
             if "model name" in line:
-                return re.sub( ".*model name.*:", "", line,1)
+                return re.sub(".*model name.*:", "", line, 1)
     return platform.processor()
