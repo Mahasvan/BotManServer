3c88fb615b392a8dbbe2c28a85c6c32fb700d879
Mahas1 <mahasvan@gmail.com>
2023-06-26 20:07:09 +0530

    add host and port keys to config.json

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ for route in routes:
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host=config.get("host"), port=config.get("port"))
