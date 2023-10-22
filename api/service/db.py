23c0cefb100ee2e5b0f0760fd2c52dab99568620
Mahas1 <mahasvan@gmail.com>
2023-10-22 12:12:55 +0530

    make db a singleton class, rename variables

--- api/service/db.py
+++ api/service/db.py
@@ -3,27 +3,37 @@ import sqlalchemy as db
 from sqlalchemy import Table, create_engine, Column
 from sqlalchemy.orm import Session
 
+
 class Database:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if Database._instance is None:
+            Database._instance = object.__new__(cls)
+            Database._instance.__init__(*args, **kwargs)
+
+        return Database._instance
+
     def __init__(self, dbfile: str = "log.db"):
         self.engine = create_engine(f"sqlite:///{dbfile}")
         self.engine.connect()
         self.meta = db.MetaData()
 
-        self.errors = Table(
+        self._errors = Table(
             "errors", self.meta,
             Column("file", db.String),
             Column("error_type", db.String),
             Column("error", db.String),
             Column("time", db.DateTime))
 
-        self.warnings = Table(
+        self._warnings = Table(
             "warnings", self.meta,
             Column("file", db.String),
             Column("warning", db.String),
             Column("time", db.DateTime)
         )
 
-        self.infos = Table(
+        self._info = Table(
             "info", self.meta,
             Column("file", db.String),
             Column("info", db.String),
@@ -34,8 +44,8 @@ class Database:
 
     def error(self, error: Exception, file_or_context: str = "N/A"):
         with Session(self.engine) as session:
-            command = self.errors.insert().values(
-                error_type = type(error).__name__,
+            command = self._errors.insert().values(
+                error_type=type(error).__name__,
                 error=str(error),
                 file=file_or_context,
                 time=db.func.now()
@@ -45,7 +55,7 @@ class Database:
 
     def warning(self, warning: str, file_or_context: str = "N/A"):
         with Session(self.engine) as session:
-            command = self.warnings.insert().values(
+            command = self._warnings.insert().values(
                 warning=warning,
                 file=file_or_context,
                 time=db.func.now()
@@ -55,7 +65,7 @@ class Database:
 
     def info(self, info: str, file_or_context: str = "N/A"):
         with Session(self.engine) as session:
-            command = self.infos.insert().values(
+            command = self._info.insert().values(
                 info=info,
                 file=file_or_context,
                 time=db.func.now()
